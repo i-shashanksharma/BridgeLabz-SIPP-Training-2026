788538ec915e32ed0a8b50a29859347770f1f54f
i-shashanksharma <shashanksharma240602005@gmail.com>
2026-06-29 10:24:39 +0530

    [Shashank] add: Control-Flow

---
+++ core-java-practice/gcr-codebase/control-flow/Level-3/BMICalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double weight = sc.nextDouble();
+        double heightCm = sc.nextDouble();
+
+        double heightMeter = heightCm / 100;
+        double bmi = weight / (heightMeter * heightMeter);
+
+        String status;
+
+        if (bmi <= 18.4) {
+            status = "Underweight";
+        } else if (bmi <= 24.9) {
+            status = "Normal";
+        } else if (bmi <= 39.9) {
+            status = "Overweight";
+        } else {
+            status = "Obese";
+        }
+
+        System.out.println("BMI = " + bmi);
+        System.out.println("Status = " + status);
+    }
+}
