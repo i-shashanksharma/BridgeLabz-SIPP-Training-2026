54c2558c59c2d4785060fec687d72ae10b8dd569
i-shashanksharma <shashanksharma240602005@gmail.com>
2026-07-16 11:18:37 +0530

    [Shashank] add: Sliding Window And Two Pointers

---
+++ core-java-practice/gcr-codebase/Two-Pointers-And-Sliding-Window/FraudDetectionTransactionLogs.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class FraudDetectionTransactionLogs {
+    public static int[] findPairSum(int[] transactions, int target) {
+        int left = 0;
+        int right = transactions.length - 1;
+
+        while (left < right) {
+            int sum = transactions[left] + transactions[right];
+
+            if (sum == target)
+                return new int[]{transactions[left], transactions[right]};
+            else if (sum < target)
+                left++;
+            else
+                right--;
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] transactions = {1000, 2000, 3000, 4000, 4999};
+        int target = 5000;
+
+        System.out.println(Arrays.toString(findPairSum(transactions, target)));
+    }
+}
